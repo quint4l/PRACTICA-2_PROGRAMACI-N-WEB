154817aff5cc96a635567710f4a50929a11c62fb
gildardo13 <gildardo.david13@gmail.com>
2024-09-05 11:22:12 -0600

    añadido header y body pagina dos

---
+++ src/app/paginados.components.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-pagina',
+    templateUrl : './pagina.component.html',
+    styleUrls : ['./pagina.component.css'],
+})
+
+export class PaginaComponent {
+
+}
